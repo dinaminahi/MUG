f3a1e26b522aebe728df0c62fe35d49a76080a35
Liubomyra Dzhavala <lyubomyrka@ukr.net>
2020-03-10 03:34:12 +0200

    Init create basic page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { PageAboutComponent } from "./page-about/page-about.component";
+import { PageContactsComponent } from "./page-contacts/page-contacts.component";
+import { PageEventsComponent } from "./page-events/page-events.component";
+import { PageGamesComponent } from "./page-games/page-games.component";
+import { PageHomeComponent } from "./page-home/page-home.component";
+import { PageLoginComponent } from "./page-login/page-login.component";
+import { PageNewsComponent } from "./page-news/page-news.component";
+import { PageUsersComponent } from "./page-users/page-users.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+export const routes: Routes = [
+  { path: "about", component: PageAboutComponent },
+  { path: "contacts", component: PageContactsComponent },
+  { path: "events", component: PageEventsComponent },
+  { path: "games", component: PageGamesComponent },
+  { path: "home", component: PageHomeComponent },
+  { path: "login", component: PageLoginComponent },
+  { path: "news", component: PageNewsComponent },
+  { path: "users", component: PageUsersComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { PageHomeComponent } from "./page-home/page-home.component";
+import { PageAboutComponent } from "./page-about/page-about.component";
+import { PageEventsComponent } from "./page-events/page-events.component";
+import { PageGamesComponent } from "./page-games/page-games.component";
+import { PageContactsComponent } from "./page-contacts/page-contacts.component";
+import { PageNewsComponent } from "./page-news/page-news.component";
+import { PageUsersComponent } from "./page-users/page-users.component";
+import { PageLoginComponent } from "./page-login/page-login.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { LayoutHeaderComponent } from './layout-header/layout-header.component';
+import { LayoutFooterComponent } from './layout-footer/layout-footer.component';
+import { LayoutBodyComponent } from './layout-body/layout-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHomeComponent,
+    PageAboutComponent,
+    PageEventsComponent,
+    PageGamesComponent,
+    PageContactsComponent,
+    PageNewsComponent,
+    PageUsersComponent,
+    PageLoginComponent,
+    PageNotFoundComponent,
+    LayoutHeaderComponent,
+    LayoutFooterComponent,
+    LayoutBodyComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
