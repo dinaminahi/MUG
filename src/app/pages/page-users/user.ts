35fbecf1c8883d3ef81ed15a13b299be2096b5c2
Liubomyra Dzhavala <lyubomyrka@ukr.net>
2020-04-04 16:46:41 +0300

    update user class

--- src/app/pages/page-users/user.ts
+++ src/app/pages/page-users/user.ts
@@ -5,23 +5,20 @@ export class User {
     name: string;
     firstName: string;
     lastName: string;
-    phone: number;
-    email: any;
-    location: { address: string; latitude: number; longitude: number };
+    phone: string;
+    email: string;
+    location: { address: string; geo: { latitude: number; longitude: number } };
     dateOfBirth: number;
     description: string; //'Фанат кооперативних ігор; завжди беру на зустріч з собою пиво на всю компанію'
   };
   events: {
     subscribed: [];
     interested: [];
+    created: [];
   };
   games: {
     favorited: []; // game id's or some part of game objects
-    experience: {
-      novice: [];
-      middle: [];
-      advanced: [];
-    };
+    skillLevel: { novice: []; beginner: []; intermediate: []; advanced: [] };
   };
   rating: number; // likes counter form other users
 }
