ecd9d9e6a7f9c149200a57b608e374e3e252294d
Liubomyra Dzhavala <lyubomyrka@ukr.net>
2020-04-08 01:06:15 +0300

    in progres

--- src/app/add-to-favorites/add-to-favorites.component.ts
+++ src/app/add-to-favorites/add-to-favorites.component.ts
@@ -1,16 +1,41 @@
-import { Component, OnInit } from "@angular/core";
-import { DataService } from "../data.service";
+import { Component, OnInit, EventEmitter, Input, Output } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { EventItem } from "../event-item/event-item";
+import { Game } from "../game/game";
+import { User } from "../pages/page-users/user";
 import { from } from "rxjs";
+import { concatMap , tap} from 'rxjs/operators';
+import { DataService } from "../data.service";
+
 
 @Component({
   selector: "app-add-to-favorites",
   templateUrl: "./add-to-favorites.component.html",
-  styleUrls: ["./add-to-favorites.component.scss"]
+  styleUrls: ["./add-to-favorites.component.scss"],
 })
 export class AddToFavoritesComponent implements OnInit {
-  constructor(private dataService: DataService) {}
+
+  @Input() game: Game;
+  @Input() event: EventItem;
+  @Input() user: User;
+  @Output() toggle = new EventEmitter<boolean>();
+  isSubmitting = false;
+  isAuthenticated = true;
+  constructor(private http: HttpClient, private router: Router, private dataService: DataService) {}
 
   ngOnInit(): void {}
+
+  toggleFavorite() {
+    this.isSubmitting = true;
+    this.user.isAuthenticated.pipe(concatMap(
+      (authenticated) => {
+        // Not authenticated? Push to login screen
+        if (!authenticated) {
+          this.router.navigateByUrl('/login');
+          return of(null);
+        }
+
 }
 
 // Просто як приклад як можна робити:
