aaedc3582edfbb5f184f78cce3352ec2bca21bfc
Viktor Horodnychyy <viktor.horodnychyy@gmail.com>
2020-04-07 18:51:46 +0300

    auth

--- server.js
+++ server.js
@@ -1,31 +1,56 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const http = require('http');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const http = require("http");
 const app = express();
 
+const jwt = require("express-jwt");
+const jwks = require("jwks-rsa");
+const cors = require("cors");
 // API file for interacting with MongoDB
-const api = require('./server/routes/api');
+const api = require("./server/routes/api");
 
 // Parsers
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
 
 // Angular DIST output folder
-app.use(express.static(path.join(__dirname, 'dist/MUG-project')));
+app.use(express.static(path.join(__dirname, "dist/MUG-project")));
 
 // API location
-app.use('/api', api);
+app.use("/api", api);
 
 // Send all other requests to the Angular app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/MUG-project/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/MUG-project/index.html"));
 });
 
 //Set Port
-const port = process.env.PORT || '3000';
-app.set('port', port);
+const port = process.env.PORT || "3000";
+app.set("port", port);
 
 const server = http.createServer(app);
 
 server.listen(port, () => console.log(`Running on localhost:${port}`));
+
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://vic-horodnychyy.eu.auth0.com/.well-known/jwks.json",
+  }),
+  audience: "http://localhost:4200",
+  issuer: "https://vic-horodnychyy.eu.auth0.com/",
+  algorithms: ["RS256"],
+});
+app.use(jwtCheck);
+
+app.get("/authorized", function (req, res) {
+  res.send("Secured Resource");
+});
--- src/app/layout/layout-header/layout-header.component.ts
+++ src/app/layout/layout-header/layout-header.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "./../../auth.service";
 
 @Component({
-  selector: 'app-layout-header',
-  templateUrl: './layout-header.component.html',
-  styleUrls: ['./layout-header.component.scss']
+  selector: "app-layout-header",
+  templateUrl: "./layout-header.component.html",
+  styleUrls: ["./layout-header.component.scss"],
 })
 export class LayoutHeaderComponent implements OnInit {
+  constructor(public auth: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
