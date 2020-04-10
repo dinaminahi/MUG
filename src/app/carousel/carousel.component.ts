978df8a82b2127ad451f0c114267a78ad55dcebb
Liubomyra Dzhavala <lyubomyrka@ukr.net>
2020-04-10 03:51:10 +0300

    add events into carousel on home-page

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,88 +1,25 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  HostListener,
+} from "@angular/core";
 
 @Component({
   selector: "app-carousel",
   templateUrl: "./carousel.component.html",
-  styleUrls: ["./carousel.component.scss"]
+  styleUrls: ["./carousel.component.scss"],
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent implements OnInit, OnChanges {
+  @Input() events: [];
   constructor() {}
-
-  cards = [
-    {
-      title: "EVENT NAME",
-      description:
-        "Some quick example text to build on the card title and make up the bulk of the card content",
-      buttonText: "Button",
-      img:
-        "https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
-    },
-    {
-      title: "EVENT NAME",
-      description:
-        "Some quick example text to build on the card title and make up the bulk of the card content",
-      buttonText: "Button",
-      img:
-        "https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
-    },
-    {
-      title: "EVENT NAME",
-      description:
-        "Some quick example text to build on the card title and make up the bulk of the card content",
-      buttonText: "Button",
-      img:
-        "https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
-    },
-    {
-      title: "EVENT NAME",
-      description:
-        "Some quick example text to build on the card title and make up the bulk of the card content",
-      buttonText: "Button",
-      img:
-        "https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
-    },
-    {
-      title: "EVENT NAME",
-      description:
-        "Some quick example text to build on the card title and make up the bulk of the card content",
-      buttonText: "Button",
-      img:
-        "https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
-    },
-    {
-      title: "EVENT NAME",
-      description:
-        "Some quick example text to build on the card title and make up the bulk of the card content",
-      buttonText: "Button",
-      img:
-        "https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
-    },
-    {
-      title: "EVENT NAME",
-      description:
-        "Some quick example text to build on the card title and make up the bulk of the card content",
-      buttonText: "Button",
-      img:
-        "https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
-    },
-    {
-      title: "EVENT NAME",
-      description:
-        "Some quick example text to build on the card title and make up the bulk of the card content",
-      buttonText: "Button",
-      img:
-        "https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
-    },
-    {
-      title: "EVENT NAME",
-      description:
-        "Some quick example text to build on the card title and make up the bulk of the card content",
-      buttonText: "Button",
-      img:
-        "https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
-    }
-  ];
-  slides: any = [[]];
+  slides1: any = [[]];
+  slides2: any = [[]];
+  slides3: any = [[]];
+  carouselDisplayMode: number;
+  TABLET_BREAKPOINT: number = 768;
+  DESKTOP_BREAKPOINT: number = 1024;
   chunk(arr, chunkSize) {
     let R = [];
     for (let i = 0, len = arr.length; i < len; i += chunkSize) {
@@ -90,7 +27,38 @@ export class CarouselComponent implements OnInit {
     }
     return R;
   }
-  ngOnInit() {
-    this.slides = this.chunk(this.cards, 3);
+  switchLayout() {
+    if (window.innerWidth < this.TABLET_BREAKPOINT) {
+      this.carouselDisplayMode = 1;
+    } else if (
+      this.TABLET_BREAKPOINT < window.innerWidth &&
+      window.innerWidth < this.DESKTOP_BREAKPOINT
+    ) {
+      this.carouselDisplayMode = 2;
+    } else {
+      this.carouselDisplayMode = 3;
+    }
+    console.log(
+      this.carouselDisplayMode,
+      window.innerWidth,
+      this.TABLET_BREAKPOINT,
+      this.DESKTOP_BREAKPOINT
+    );
   }
+  ngOnChanges() {
+    if (this.events) {
+      this.slides1 = this.events;
+      this.slides2 = this.chunk(this.events, 2);
+      this.slides3 = this.chunk(this.events, 3);
+      this.switchLayout();
+    }
+  }
+  @HostListener("window:resize")
+  onWindowResize() {
+    if (this.events) {
+      this.switchLayout();
+    }
+  }
+
+  ngOnInit(): void {}
 }
