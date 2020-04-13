ae98d325ef298e7fdb382e1be269344de0f65083
Viktor Horodnychyy <viktor.horodnychyy@gmail.com>
2020-04-13 16:32:49 +0300

    2 server work

--- server.js
+++ server.js
@@ -1,153 +1,93 @@
-// const express = require("express");
-// const path = require("path");
-// const http = require("http");
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const app = express();
+
+// API file for interacting with MongoDB
+const api = require("./server/routes/api");
+
+// Parsers
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+// Angular DIST output folder
+app.use(express.static(path.join(__dirname, "dist/MUG-project")));
+
+// API location
+app.use("/api", api);
+
+// Send all other requests to the Angular app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/MUG-project/index.html"));
+});
+
+//Set Port
+const port = process.env.PORT || "3000";
+app.set("port", port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Running on localhost:${port}`));
+
+// -----------------------------auth---------------------
+
+// // const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+// const dbConfig = require('.server/database/db');
+
+// // Express APIs
+// const authApi = require('./server/routes/auth.routes');
+
+// // MongoDB conection
+// mongoose.Promise = global.Promise;
+// mongoose.connect(dbConfig.db, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// }).then(() => {
+//     console.log('Database connected')
+//   },
+//   error => {
+//     console.log("Database can't be connected: " + error)
+//   }
+// )
+
+// // Remvoe MongoDB warning error
+// mongoose.set('useCreateIndex', true);
+
+// // Express settings
 // const app = express();
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//   extended: false
+// }));
+// app.use(cors());
 
-// // API file for interacting with MongoDB
-// const api = require("./server/routes/api");
+// // Serve static resources
+// app.use('/public', express.static('public'));
 
-// // Parsers
-// app.use(express.json());
-// app.use(
-//   express.urlencoded({
-//     extended: false,
-//   })
-// );
+// app.use('/authApi', authApi)
 
-// // Angular DIST output folder
-// app.use(express.static(path.join(__dirname, "dist/MUG-project")));
+// // Define PORT
+// const authPort = process.env.PORT || 4000;
+// const serverAuth = app.listen(port, () => {
+//   console.log('Connected to port ' + authPort)
+// })
 
-// // API location
-// app.use("/api", api);
-
-// // Send all other requests to the Angular app
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "dist/MUG-project/index.html"));
+// // Express error handling
+// app.use((req, res, next) => {
+//   setImmediate(() => {
+//     next(new Error('Something went wrong'));
+//   });
 // });
 
-// //Set Port
-// const port = process.env.PORT || "3000";
-// app.set("port", port);
-
-// const server = http.createServer(app);
-
-// server.listen(port, () => console.log(`Running on localhost:${port}`));
-
-// // -----------------------------auth---------------------
-
-// // // const express = require('express');
-// // const mongoose = require('mongoose');
-// // const cors = require('cors');
-// // const bodyParser = require('body-parser');
-// // const dbConfig = require('.server/database/db');
-
-// // // Express APIs
-// // const authApi = require('./server/routes/auth.routes');
-
-// // // MongoDB conection
-// // mongoose.Promise = global.Promise;
-// // mongoose.connect(dbConfig.db, {
-// //   useNewUrlParser: true,
-// //   useUnifiedTopology: true
-// // }).then(() => {
-// //     console.log('Database connected')
-// //   },
-// //   error => {
-// //     console.log("Database can't be connected: " + error)
-// //   }
-// // )
-
-// // // Remvoe MongoDB warning error
-// // mongoose.set('useCreateIndex', true);
-
-// // // Express settings
-// // const app = express();
-// // app.use(bodyParser.json());
-// // app.use(bodyParser.urlencoded({
-// //   extended: false
-// // }));
-// // app.use(cors());
-
-// // // Serve static resources
-// // app.use('/public', express.static('public'));
-
-// // app.use('/authApi', authApi)
-
-// // // Define PORT
-// // const authPort = process.env.PORT || 4000;
-// // const serverAuth = app.listen(port, () => {
-// //   console.log('Connected to port ' + authPort)
-// // })
-
-// // // Express error handling
-// // app.use((req, res, next) => {
-// //   setImmediate(() => {
-// //     next(new Error('Something went wrong'));
-// //   });
-// // });
-
-// // app.use(function (err, req, res, next) {
-// //   console.error(err.message);
-// //   if (!err.statusCode) err.statusCode = 500;
-// //   res.status(err.statusCode).send(err.message);
-// // });
-
-// ----------------------------
-
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const dbConfig = require('./server/database/db');
-
-// Express APIs
-const authApi = require('./server/routes/auth.routes');
-
-// MongoDB conection
-mongoose.Promise = global.Promise;
-mongoose.connect(dbConfig.db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-    console.log('Database connected')
-  },
-  error => {
-    console.log("Database can't be connected: " + error)
-  }
-)
-
-// Remvoe MongoDB warning error
-mongoose.set('useCreateIndex', true);
-
-
-// Express settings
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(cors());
-
-// Serve static resources
-app.use('/public', express.static('public'));
-
-app.use('/authApi', authApi)
-
-// Define PORT
-const port = process.env.PORT || 4000;
-const server = app.listen(port, () => {
-  console.log('Connected to port ' + port)
-})
-
-// Express error handling
-app.use((req, res, next) => {
-  setImmediate(() => {
-    next(new Error('Something went wrong'));
-  });
-});
-
-app.use(function (err, req, res, next) {
-  console.error(err.message);
-  if (!err.statusCode) err.statusCode = 500;
-  res.status(err.statusCode).send(err.message);
-});
+// app.use(function (err, req, res, next) {
+//   console.error(err.message);
+//   if (!err.statusCode) err.statusCode = 500;
+//   res.status(err.statusCode).send(err.message);
+// });
