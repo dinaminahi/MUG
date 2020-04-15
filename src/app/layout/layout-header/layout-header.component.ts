37a7596f2544f1d49430ef097ad368bb16d9dae0
Viktor Horodnychyy <viktor.horodnychyy@gmail.com>
2020-04-15 10:51:19 +0300

    scrol + routing aad

--- src/app/layout/layout-header/layout-header.component.ts
+++ src/app/layout/layout-header/layout-header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService } from "./../../shared/auth.service";
+import { Router } from "@angular/router";
+import { NgxPageScrollModule } from "ngx-page-scroll";
 
 @Component({
   selector: "app-layout-header",
@@ -7,10 +9,18 @@ import { AuthService } from "./../../shared/auth.service";
   styleUrls: ["./layout-header.component.scss"],
 })
 export class LayoutHeaderComponent implements OnInit {
-  constructor(public authService: AuthService) {}
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+    private pageScroll: NgxPageScrollModule
+  ) {}
 
   logout() {
     this.authService.doLogout();
+    this.router.navigate(["/home"]);
+  }
+  Scrolldown() {
+    this.pageScroll;
   }
   ngOnInit(): void {}
 }
