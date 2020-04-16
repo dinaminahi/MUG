c0ec471a8b646c2e7050b20f2be4a39f188b525b
Viktor Horodnychyy <viktor.horodnychyy@gmail.com>
2020-04-16 14:59:12 +0300

    pop up dialog

--- src/app/layout/layout-main-navbar/layout-main-navbar.component.ts
+++ src/app/layout/layout-main-navbar/layout-main-navbar.component.ts
@@ -2,6 +2,18 @@ import { Component, OnInit } from "@angular/core";
 import { AuthService } from "./../../shared/auth.service";
 import { Router } from "@angular/router";
 import { NgxPageScrollModule } from "ngx-page-scroll";
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from "@angular/material/dialog";
+import { SigninComponent } from "./../../components/signin/signin.component";
+
+export interface DialogData {
+  name: any;
+  password: any;
+  email: any;
+}
 
 @Component({
   selector: "app-layout-main-navbar",
@@ -9,11 +21,32 @@ import { NgxPageScrollModule } from "ngx-page-scroll";
   styleUrls: ["./layout-main-navbar.component.scss"],
 })
 export class LayoutMainNavbarComponent implements OnInit {
+  name: any;
+  password: any;
+  email: any;
+
   constructor(
     public authService: AuthService,
     private router: Router,
-    private pageScroll: NgxPageScrollModule
+    private pageScroll: NgxPageScrollModule,
+    public dialog: MatDialog
   ) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(SigninComponent, {
+      width: "400px",
+      data: {
+        name: this.name,
+        password: this.password,
+        email: this.password,
+      },
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      alert("Are You Shoore?");
+      this.name = result;
+    });
+    // this.dialog.open(SigninComponent);
+  }
 
   logout() {
     this.authService.doLogout();
