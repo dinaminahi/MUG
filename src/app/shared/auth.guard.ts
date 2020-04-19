d386ca73606894becf983057a869a7a229e14f37
Diana Grechukh <diankagrechukh@gmail.com>
2020-04-19 12:55:26 +0300

    add functionality to create event btn

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -8,21 +8,44 @@ import {
 } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "./../shared/auth.service";
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from "@angular/material/dialog";
+import { SigninComponent } from "./../components/signin/signin.component";
+
+export interface DialogData {
+  name: any;
+  password: any;
+  email: any;
+}
 
 @Injectable({
   providedIn: "root",
 })
+
 export class AuthGuard implements CanActivate {
-  constructor(public authService: AuthService, public router: Router) {}
+  constructor(public authService: AuthService, public router: Router, public dialog: MatDialog) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
     if (this.authService.isLoggedIn !== true) {
-      window.alert("Access not allowed!");
-      this.router.navigate(["log-in"]);
+      const dialogRef = this.dialog.open(SigninComponent, {
+        width: "450px",
+      });
+      dialogRef.afterClosed().subscribe((result) => {
+
+      });
     }
     return true;
+  }
+
+
+  openDialog(): void {
+    
+    // this.dialog.open(SigninComponent);
   }
 }
