e0529c5b8af98b8823537602f74c64ccbe1711ff
Liubomyra Dzhavala <lyubomyrka@ukr.net>
2020-04-21 20:16:29 +0300

    users photo on event only if this.authService.UserId

--- src/app/participants-count/participants-count.component.ts
+++ src/app/participants-count/participants-count.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Router } from "@angular/router";
 import { ParticipantsCount } from "./participants-count";
+import { AuthService } from "../shared/auth.service";
 import mongoose from "mongoose";
 
 @Component({
@@ -12,7 +13,11 @@ import mongoose from "mongoose";
 export class ParticipantsCountComponent {
   @Input() count: ParticipantsCount;
   @Input() joined: any;
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    public authService: AuthService
+  ) {}
 
   onSelect(user) {
     this.router.navigate(["/user-profile", user._id]);
