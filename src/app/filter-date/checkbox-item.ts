2e2987ca172f735378ed01c69abbb7845b9a0cc3
Liubomyra Dzhavala <lyubomyrka@ukr.net>
2020-04-24 05:32:56 +0300

    add some styles for filters

--- src/app/filter-date/checkbox-item.ts
+++
@@ -1,7 +0,0 @@
-export class CheckboxItem {
-  constructor(
-    public value: string,
-    public label: string,
-    public checked?: boolean
-  ) {}
-}
--- src/app/filter-date/filter-date.component.ts
+++ src/app/filter-date/filter-date.component.ts
@@ -1,10 +1,10 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core";
-import { CheckboxItem } from "./checkbox-item";
+import { CheckboxItem } from "../filter-category/checkbox-item";
 
 @Component({
   selector: "app-filter-date",
   templateUrl: "./filter-date.component.html",
-  styleUrls: ["./filter-date.component.scss"],
+  styleUrls: ["../filter-category/filter-category.component.scss"],
 })
 export class FilterDateComponent {
   @Input() datesFormatted: string[];
