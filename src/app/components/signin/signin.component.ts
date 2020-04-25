0e040cb53d1559796a9a24d3f0f1223f376e5440
Diana Grechukh <diankagrechukh@gmail.com>
2020-04-26 01:39:37 +0300

    switch panel

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit, Inject } from "@angular/core";
-import { FormBuilder, FormGroup } from "@angular/forms";
-import { AuthService } from "./../../shared/auth.service";
-import { Router } from "@angular/router";
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from './../../shared/auth.service';
+import { Router } from '@angular/router';
 import {
   MatDialog,
   MatDialogRef,
-  MAT_DIALOG_DATA,
-} from "@angular/material/dialog";
+  MAT_DIALOG_DATA
+} from '@angular/material/dialog';
 
 @Component({
-  selector: "app-signin",
-  templateUrl: "./signin.component.html",
-  styleUrls: ["./signin.component.scss"],
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
   signinForm: FormGroup;
@@ -23,49 +23,55 @@ export class SigninComponent implements OnInit {
     public router: Router
   ) {
     this.signinForm = this.fb.group({
-      email: [""],
-      password: [""],
+      email: [''],
+      password: ['']
     });
     this.signupForm = this.fb.group({
-      name: [""],
-      email: [""],
-      password: [""],
-      repassword: [""],
+      name: [''],
+      email: [''],
+      password: [''],
+      repassword: ['']
     });
   }
 
   onClickCheck(): void {}
 
   onNoClick(): void {
-    this.router.navigate(["/home"]);
+    this.router.navigate(['/home']);
 
     // this.dialogRef.close();
   }
   ngOnInit(): void {
-    const signUpButton = document.getElementById("signUp");
-    const signInButton = document.getElementById("signIn");
-    const container = document.getElementById("container");
+    const signUpButton = document.getElementById('signUp');
 
-    signUpButton.addEventListener("click", () => {
-      container.classList.add("right-panel-active");
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
+
+    signUpButton.addEventListener('click', () => {
+      container.classList.add('right-panel-active');
     });
 
-    signInButton.addEventListener("click", () => {
-      container.classList.remove("right-panel-active");
+    signInButton.addEventListener('click', () => {
+      container.classList.remove('right-panel-active');
+    });
+
+    const signUpButtonSubmit = document.getElementById('submit-up');
+    signUpButtonSubmit.addEventListener('click', () => {
+      container.classList.remove('right-panel-active');
     });
   }
 
   loginUser() {
     this.authService.signIn(this.signinForm.value);
-    this.router.navigate(["events"]);
+    this.router.navigate(['events']);
   }
 
   registerUser() {
-    this.authService.signUp(this.signupForm.value).subscribe((res) => {
+    this.authService.signUp(this.signupForm.value).subscribe(res => {
       if (res.result) {
         console.log(res);
         this.signupForm.reset();
-        this.router.navigate(["user-profile/" + res.msg._id]);
+        this.router.navigate(['user-profile/' + res.msg._id]);
         // this.router.navigate(["sign-in"]);
       }
     });
