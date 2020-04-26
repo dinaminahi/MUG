109614864af317eab7ff09a543766e0defc0c7c0
Viktor Horodnychyy <viktor.horodnychyy@gmail.com>
2020-04-26 07:15:28 +0300

    testng

--- src/app/layout/layout-contacts/layout-contacts.component.ts
+++ src/app/layout/layout-contacts/layout-contacts.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-layout-contacts',
-  templateUrl: './layout-contacts.component.html',
-  styleUrls: ['./layout-contacts.component.scss']
+  selector: "app-layout-contacts",
+  templateUrl: "./layout-contacts.component.html",
+  styleUrls: ["./layout-contacts.component.scss"],
 })
 export class LayoutContactsComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    (function () {
+      "use strict";
 
+      // define variables
+      var items = document.querySelectorAll(".timeline li");
+
+      // check if an element is in viewport
+      // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+      function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <=
+            (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.right <=
+            (window.innerWidth || document.documentElement.clientWidth)
+        );
+      }
+
+      function callbackFunc() {
+        for (var i = 0; i < items.length; i++) {
+          if (isElementInViewport(items[i])) {
+            items[i].classList.add("in-view");
+          }
+        }
+      }
+
+      // listen for events
+      window.addEventListener("load", callbackFunc);
+      window.addEventListener("resize", callbackFunc);
+      window.addEventListener("scroll", callbackFunc);
+    })();
+  }
 }
---
+++ src/app/layout/layout-time-line/layout-time-line.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-time-line',
+  templateUrl: './layout-time-line.component.html',
+  styleUrls: ['./layout-time-line.component.scss']
+})
+export class LayoutTimeLineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
