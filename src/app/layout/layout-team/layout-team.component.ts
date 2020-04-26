96ab45644ac474d826b83aeb9a42ad74a22e8536
Viktor Horodnychyy <viktor.horodnychyy@gmail.com>
2020-04-26 06:05:09 +0300

    team done

--- src/app/layout/layout-team/layout-team.component.ts
+++ src/app/layout/layout-team/layout-team.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-layout-team',
-  templateUrl: './layout-team.component.html',
-  styleUrls: ['./layout-team.component.scss']
+  selector: "app-layout-team",
+  templateUrl: "./layout-team.component.html",
+  styleUrls: ["./layout-team.component.scss"],
 })
 export class LayoutTeamComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
