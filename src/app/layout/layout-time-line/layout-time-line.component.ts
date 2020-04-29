003fb54a28e9369e907c4cb7c55a188ea435856f
Viktor Horodnychyy <viktor.horodnychyy@gmail.com>
2020-04-29 08:37:14 +0300

    xeperemental changs

--- src/app/layout/layout-time-line/layout-time-line.component.ts
+++ src/app/layout/layout-time-line/layout-time-line.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-layout-time-line',
-  templateUrl: './layout-time-line.component.html',
-  styleUrls: ['./layout-time-line.component.scss']
+  selector: "app-layout-time-line",
+  templateUrl: "./layout-time-line.component.html",
+  styleUrls: ["./layout-time-line.component.scss"],
 })
 export class LayoutTimeLineComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
