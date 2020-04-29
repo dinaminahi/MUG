1f96fd350922707ea104ec08819b800e2185f2a9
Viktor Horodnychyy <viktor.horodnychyy@gmail.com>
2020-04-29 14:17:17 +0300

    news home page

--- src/app/pages/page-news/page-news.component.ts
+++ src/app/pages/page-news/page-news.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-page-news',
-  templateUrl: './page-news.component.html',
-  styleUrls: ['./page-news.component.scss']
+  selector: "app-page-news",
+  templateUrl: "./page-news.component.html",
+  styleUrls: ["./page-news.component.scss"],
 })
 export class PageNewsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
